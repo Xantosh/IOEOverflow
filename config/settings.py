22a7cffdcb17e9a27a31398ab450a0f019b01690
VectorTensor <comppro54@gmail.com>
2022-01-05 09:10:32 +0545

    change in plans

--- config/settings.py
+++ config/settings.py
@@ -38,13 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'django_elasticsearch_dsl'
 ]
-ELASTICSEARCH_DSL ={
-        'default':{
-            'hosts' : 'localhost:9200'
-            }
-        }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
