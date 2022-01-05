bef68654ab7a2e4f1259bb84e19f0981c378bd4e
VectorTensor <comppro54@gmail.com>
2022-01-05 11:06:40 +0545

    added models and sample elastic search helper function

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=1000)
+    image = models.ImageField(null = True)
+    upvote= models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from elasticsearch import Elasticsearch
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,23 @@ def index(request):
 
 def forum(request):
     return render(request,"main/forum.html")
+
+# Posting question view 
+def questionPost(request):
+    # First, count the total number of documents in the index in the elastic search
+    # Second, update the models use the id from the total number of contents +1 
+    # Third, update the data in the elastic server database
+    pass
+
+def update_els_server(id,text,image):
+    es = Elasticsearch()
+    doc ={
+            'text':text,
+            'image':image
+            } 
+    es.create(index='question',id=id,body=doc)
+
+
+
+
+
