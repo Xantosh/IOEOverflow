67dcb43f9b8a3134f39c2083befe5892ac9e010e
Santosh Upreti <69950069+Xantosh@users.noreply.github.com>
2022-01-26 12:38:42 +0545

    changed to load index.html at start of server

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from main.views import index , forum,questionPost,search
 
 urlpatterns = [
-        path('main/',index,name="index"),
+        path('',index,name="index"),
         path('forum/',forum,name="forum"),
         path('posts/',questionPost,name="posts"),
         path('search/',search,name="search")
